04e1b7d091ec2ec1bc11c4b5005388e7558afb74
SilvioBruno <160060640+SilvioBruno@users.noreply.github.com>
2026-03-12 20:24:18 -0300

    Add files via upload
    plotagi

--- Regression.py
+++ Regression.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 class RegressionM: #construtor
     def __init__(self, X, y):
@@ -28,8 +29,9 @@ X = np.column_stack((x1,x2))
     
 modelo = RegressionM(X, y)
 
-modelo.fit()
+modelo.fit() #treinamento
 y_pred = modelo.predict(X) #predizer os valores de y
+
 print(y_pred)
 
 def r2_score(y_true, y_prediction):
@@ -37,11 +39,37 @@ def r2_score(y_true, y_prediction):
     denominador = np.sum((y_true - np.mean(y_true))**2)
     r2 = 1 - (numerador/denominador)
     return r2
+
 print(r2_score(y, y_pred))
 
+## Plotagem
 
-    
-    
+print(np.linspace(min(x1), max(x1), 20))
+print(np.linspace(min(x2), max(x2), 20)) 
+
+x1_grid, x2_grid = np.meshgrid(np.linspace(min(x1), max(x1), 20), 
+                               np.linspace(min(x2), max(x2), 20))
+
+y_grid = modelo.beta[0] + modelo.beta[1]*x1_grid + modelo.beta[2]*x2_grid
+
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+ax.scatter(x1,x2,y,color="red", label="Dados Reais")
+#plano da regressão
+
+ax.plot_surface(x1_grid,x2_grid,y_grid,alpha=0.5, color = "blue")
+plt.show()
+  
+
+
+### plotagem
+
+fig = go.Figure()
+fig.add_scatter3d(x=x1,y=x2,z=y, mode = "markers", marker=dict(color="red", size=3), name = "dados originais")  
+fig.add_scatter3d(x=x1,y=x2,z=y_pred, mode = "markers", marker = dict(color="green", size=3), name = "dados preditos")
+
+fig.add_surface(x=x1_grid, y=x2_grid,z=y_grid, opacity=0.5)
+fig.show()
     
     
     
