8a9b5314f237c65b55a973e52227ed6d4d98b200
SilvioBruno <160060640+SilvioBruno@users.noreply.github.com>
2026-03-17 19:59:50 -0300

    Add files via upload

--- hold_out.py
+++ hold_out.py
@@ -1,9 +1,33 @@
 import numpy as np
+import math
+
 x = np.array([34,5,6,4])
 random_state = 42
 np.random.seed(random_state)
 print(np.random.permutation(x)) #embaralhar
-
-
+tes_size = 0.3 
+n_samples = len(x)
+n_test = math.ceil(n_samples * tes_size)#arrendonda pra cima
+print(n_test)
+def train_test_split(X,y,test_size=0.3, random_state=42):
+    if random_state is not None:
+        np.random.seed(random_state)
+    if len(X) != len(y):
+        raise ValueError("X e y devem ter o mesmo tamanho")
+    print("Quantidade da amostra", n_samples)
+    indices = np.random.permutation(n_samples)
+    print("Indices embaralhados", indices) 
+    n_test = math.ceil(n_samples * test_size)
+    print("Quantidade de amostras para teste", n_test)
+    test_indices = indices[:n_test]
+    print("Dados de test", test_indices)
+    train_indices = indices[n_test:]
+    print("Dados de treino",train_indices)
+    if X.ndim == 1:
+        X_train, X_test = X_train[train_indices], X_test[test_indices]
+    else:
+        X_train, X_test = X_train[train_indices,:], X_test[test_indices,:]
+    y_train, y_test = y[train_indices], y[test_indices]
+    X_train, X_test , y_train, y_test
 
 #preste atenção, é o meu cão...
